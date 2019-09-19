d42eaa6a849d2b7e8ebb03399a4e5e03579e59d4
郑国鹏 <502785507@qq.com>
2019-09-19 14:43:10 +0800

    'city-layout'

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="item">
+    <div class="item-list">A</div>
+    <div class="item-list">A</div>
+    <div class="item-list">A</div>
+    <div class="item-list">A</div>
+    <div class="item-list">A</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+.item
+  display: flex
+  flex-direction: column
+  justify-content: center
+  position: absolute
+  width: .44rem
+  top: 1.58rem
+  right: 0
+  bottom: 0
+  color: #00bcd4
+  .item-list
+    height: .4rem
+    text-align: center
+</style>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+      </div>
+          <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+      </div>
+          <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+      </div>
+          <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+        <div class="item border-bottom">阿亚历山大</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.border-topbottom
+  &:before
+    border-color: #ccc
+  &:after
+    border-color: #ccc
+.border-bottom
+  &:before
+    border-color: #ccc
+.list
+  position:absolute
+  overflow: hidden
+  top: 1.58rem
+  left: 0
+  right: 0
+  bottom: 0
+  .title
+    height: .54rem
+    line-height: .54rem
+    font-size: .28rem
+    text-indent: .2rem
+    color: #666
+    background: #eee
+  .button-list
+    padding: .1rem .6rem .1rem .1rem
+    overflow: hidden
+    .button-wrapper
+      float: left
+      width: 33.33%
+      .button
+        text-align: center
+        margin: .1rem
+        padding: .1rem 0
+        border: .02rem solid #ccc
+        border-radius: .06rem
+  .item
+    height: .72rem
+    line-height: .72rem
+    padding-left: .2rem
+</style>
