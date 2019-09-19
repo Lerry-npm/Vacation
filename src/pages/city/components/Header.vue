305467ee8f5e83024dae76e81eba5122e090f779
郑国鹏 <502785507@qq.com>
2019-09-19 13:30:31 +0800

    'city-header'

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="header-text">城市选择</div>
+    <router-link to="/">
+      <div class="iconfont Cback">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  position: relative
+  overflow: hidden
+  height: .86rem
+  line-height: .86rem
+  background: #00bcd4
+  color: #fff
+  text-align: center
+  font-size: .32rem
+  .Cback
+    position: absolute
+    top: 0
+    left: .2rem
+    font-size: .4rem
+    text-align: center
+    color: #fff
+</style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+.search
+  height: .72rem
+  line-height: .72rem
+  background: #00bcd4
+  color: #fff
+  .search-input
+    box-sizing: border-box
+    width: 95%
+    margin-left: .15rem
+    height: .52rem
+    line-height: .52rem
+    padding: 0 .1rem
+    text-align: center
+    border-radius: .06rem
+    color: #666
+</style>
