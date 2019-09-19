c1c491a2ef301db327f6724e70a2796742685562
郑国鹏 <502785507@qq.com>
2019-09-19 20:22:03 +0800

    'city-finish'

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '上海'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (e) {}
+
 export default new Vuex.Store({
   state: {
-    city: '北京'
+    city: defaultCity
   },
   mutations: {
     changeCity (state, city) {
       state.city = city
+      try {
+        localStorage.city = city
+      } catch (e) {}
     }
   }
 })
